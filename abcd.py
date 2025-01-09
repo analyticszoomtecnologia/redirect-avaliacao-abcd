f2eeebc2b57c9df429fb631ee9d411b911cc9f6c
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2025-01-08 23:06:29 -0300

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -57,16 +57,17 @@ def buscar_colaboradores():
     cursor = connection.cursor()
     cursor.execute("""
         SELECT
-            fz.id AS id_employee,
-            fz.Nome AS nm_employee,
-            fz.Setor AS nm_departament,
-            fz.Gestor_Direto AS nm_gestor,
-            fz.Diretor_Gestor AS nm_diretor,
-            fz.Diretoria AS nm_diretoria
+          fz.id AS id_employee,
+          fz.Nome AS nm_employee,
+          fz.Setor AS nm_departament,
+          fz.Gestor_Direto AS nm_gestor,
+          fz.Diretor_Gestor AS nm_diretor,
+          fz.Diretoria AS nm_diretoria
         FROM datalake.silver_pny.func_zoom fz
         JOIN datalake.avaliacao_abcd.login lt
         ON fz.Diretor_Gestor = lt.Nome
-        WHERE lt.id_emp = '{id_emp}';
+        WHERE lt.id_emp = {id_emp}
+        ORDER BY fz.Nome ASC;
     """)
     colaboradores = cursor.fetchall()
     cursor.close()
