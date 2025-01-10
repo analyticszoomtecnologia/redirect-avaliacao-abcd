23affaaa8a7019ed6884f39c89575dbd7eeb68dc
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2025-01-09 23:42:21 -0300

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -435,7 +435,7 @@ def abcd_page():
     
 
     # Verifica se o colaborador selecionado é subordinado do gestor logado
-    if nome_colaborador and id_emp in subordinados_data:
+    #if nome_colaborador and id_emp in subordinados_data:
         # Avaliação comportamental e técnica
         notas_categorias = {}
 
@@ -493,8 +493,8 @@ def abcd_page():
                 atualizar_banco_dados(id_emp, nome_colaborador, nome_gestor, setor, diretoria, data_resposta, nota_final, soma_final, notas_categorias)
                 limpar_campos()
 
-    elif nome_colaborador:
-        st.error(f"O colaborador {nome_colaborador} não é subordinado ao gestor logado. Avaliação não permitida.")
+    #elif nome_colaborador:
+        #st.error(f"O colaborador {nome_colaborador} não é subordinado ao gestor logado. Avaliação não permitida.")
 
 
     # Lista de IDs de supervisores permitidos
