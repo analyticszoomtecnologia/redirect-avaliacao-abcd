449759380b2b9db06dbe8a120f66a8b76e08210e
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2025-01-10 09:31:22 -0300

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -439,7 +439,8 @@ def abcd_page():
                 WHERE
                    id = %s
         """ % (user_id))
-        name_user = dict(name_user.fetchall())
+        name_user = name_user.fetchall()
+        name_user = {row['id']: row['Nome'] for row in name_user}
         cursor.close()
         connection.close()
         nome_diretor = st.text_input("Diretor(a) Responsável", value=name_user, disabled=True)
